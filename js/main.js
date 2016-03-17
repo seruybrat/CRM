fd24a97cf76a493cccbba04bcb22da88d5f8ac95
seruybrat <sergiibartkiv@gmail.com>
2016-03-17 12:26:00 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
-var config = {
-   //    'DOCUMENT_ROOT':'http://vocrm.org/',
-    'DOCUMENT_ROOT': 'http://5.101.119.32:8008/',
-    'pagination_count': 50, //Количество записей при пагинации
-    'pagination_mini_count': 10
-}
-
-
-/*Боковое меню*/
 $("#nav-sidebar li ").click(function(e) {
   e.preventDefault();
   $("#nav-sidebar li").removeClass('active');
   $(this).addClass('active');
 });
 
-$(".pag li ").click(function(e) {
+
+
+$("#pag li ").click(function(e) {
   e.preventDefault();
-  $(".pag li").removeClass('active');
+  $("#pag li").removeClass('active');
   $(this).addClass('active');
 });
+
+
 
 $("#move-sidebar").click(function(){
   $("#sidebar").toggleClass("toggle-sidebar");
 });
 
+
+
+$("#sort-on").click(function () {
+      $(".table-sorting").animate({right: '0px'})
+ });
+
+
+
+$("#sort-off").click(function () {
+      $(".table-sorting").animate({right: '-300px'})
+ }); 
